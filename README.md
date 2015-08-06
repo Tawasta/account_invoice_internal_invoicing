3b026267cd1922e681a7cc39b072a11de3500650
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-06 11:52:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Account Invoice Internal Invoicing
+--------------------------
+
+Allows different income and expense accounts to be used when
+invoicing within the company group.
+
+Features
+--------
+
+* Adds internal income and expense accounts for products
+* Adds a possibility to create an internal invoice
---
+++ __openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015 - Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Account Invoice Internal Invoicing',
+    'category': 'Account',
+    'version': '0.1',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'website': 'http://www.tawasta.fi',
+    'depends': ['account'],
+    'description': '''
+Account Invoice Internal Invoicing
+--------------------------
+
+Allows different income and expense accounts to be used when
+invoicing within the company group.
+
+Features
+--------
+
+* Adds internal income and expense accounts for products
+* Adds a possibility to create an internal invoice
+''',
+    'data': [
+
+    ],
+}
