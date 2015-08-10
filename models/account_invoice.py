0b6d2b5226371715797c073b60fc649f44186b71
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-10 14:32:15 +0300

    Add an onchange method

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -7,7 +7,23 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     internal_invoice = fields.Boolean('Internal invoice')
+    internal_invoice_shown = fields.Boolean('Internal invoice shown',
+                                            default=False)
 
-    @api.onchange('partner_id')
+    ''' We are using the old api as it has loads of onchange-functions
+    that would need to be rewritten'''
+    @api.v7
+    def onchange_partner_id(self, cr, uid, ids, partner_type, partner_id,
+                            date_invoice=False, payment_term=False,
+                            partner_bank_id=False, company_id=False):
+
+        # Update the internal invoice field
+        self.onchange_partner()
+
+        return super(AccountInvoice, self).\
+            onchange_partner_id(cr, uid, ids, type, partner_id, date_invoice,
+                                payment_term, partner_bank_id, company_id)
+
+    @api.one
     def onchange_partner(self):
         pass
