6fa6db13d2125a559e99de2f6d5e5fbf084fb6c7
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-11 11:37:47 +0300

    Improve field names

--- models/product_category.py
+++ models/product_category.py
@@ -6,14 +6,14 @@ class ProductCategory(models.Model):
 
     _inherit = 'product.category'
 
-    property_account_income_categ_internal = fields.Many2one(
+    property_account_income_internal_categ = fields.Many2one(
         "account.account",
         "Internal Income Account",
         company_dependent=True,
         domain=[("type", '=', "other")],
     )
 
-    property_account_expense_categ_internal = fields.Many2one(
+    property_account_expense_internal_categ = fields.Many2one(
         "account.account",
         "Internal Expense Account",
         company_dependent=True,
