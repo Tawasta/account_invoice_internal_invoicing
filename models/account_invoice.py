8438d9f9242b29a30dd97bca64b38271aa8ea610
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-11 10:58:44 +0300

    Fix the onchange

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -18,40 +18,23 @@ class AccountInvoice(models.Model):
             default=False,
         )
 
-    ''' We are using the old api as it has loads of onchange-functions
-    that would need to be rewritten otherwise.
+    @api.multi
+    def onchange_partner_id(self, type, partner_id, date_invoice=False,
+                            payment_term=False, partner_bank_id=False,
+                            company_id=False):
 
-    TODO: This functionality WILL STOP WORKING when the account form 
-    partner_id onchange will be upgraded '''
-    @api.v7
-    def onchange_partner_id(self, cr, uid, ids, partner_type, partner_id,
-                            date_invoice=False, payment_term=False,
-                            partner_bank_id=False, company_id=False):
-
-        res = super(AccountInvoice, self).\
-            onchange_partner_id(cr, uid, ids, type, partner_id, date_invoice,
-                                payment_term, partner_bank_id, company_id)
-
-        # Update internal invoice fields
-        vals = self.onchange_partner(cr, uid, ids, partner_id)
-        if vals:
-            res['value'].update(
-                vals[0]['value']
-            )
-
-        return res
-
-    @api.one
-    def onchange_partner(self, partner_id):
-        partners = self.get_internal_partners()[0]
+        partners = self.get_internal_partners()
 
         if partner_id in partners:
             self.internal_invoice_shown = True
             self.internal_invoice = True
-
         else:
             self.internal_invoice_shown = False
             self.internal_invoice = False
+
+        res = super(AccountInvoice, self).\
+            onchange_partner_id(type, partner_id, date_invoice,
+                                payment_term, partner_bank_id, company_id)
 
         # Supporting the old API
         res = {'value': {}}
@@ -60,8 +43,8 @@ class AccountInvoice(models.Model):
 
         return res
 
-    @api.one
     def get_internal_partners(self):
+        # Get internal partner ids
         companies = self.env['res.company'].search([])
 
         partners = []
